985d75d5102c8d28b991c2455c5496d93ec48f52
Chris Lee <chrisli323@gmail.com>
2025-03-03 00:37:33 -0800

    [Feature] Create Location model

---
+++ Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftfulMapApp
+//
+//  Created by Chris Lee on 2/22/25.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
