33d0cca809572ef01aecbd01dcd6f7a0c92aa232
Chris Lee <chrisli323@gmail.com>
2025-03-10 04:54:35 -0700

    [Clean] move binding mapPosition var to LocationsViewModel. add updateMapPosition function to update map dynamically in LocationsView

--- ViewModels/LocationsViewModel.swift
+++ ViewModels/LocationsViewModel.swift
@@ -6,18 +6,39 @@
 //
 
 import Foundation
+import MapKit
+import _MapKit_SwiftUI
 
 class LocationsViewModel: ObservableObject {
     
-    // All loaded locations
+    // All loaded Location objects
     @Published var locations: [Location] = []
     
+    // Current location
     @Published var mapLocation: Location
+    
+    // Coordinate and span positioning data from Location object
+    @Published var mapPosition: MapCameraPosition
+    
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
         self.mapLocation = locations.first!
+        
+        // Initialize mapPosition with the first location
+        let region = MKCoordinateRegion(
+            center: locations.first!.coordinates,
+            span: mapSpan)
+        self.mapPosition = MapCameraPosition.region(region)
     }
     
+    private func updateMapPosition(location: Location) {
+        let region = MKCoordinateRegion(
+            center: location.coordinates,
+            span: mapSpan
+        )
+        self.mapPosition = MapCameraPosition.region(region)
+    }
 }
--- Views/LocationsView.swift
+++ Views/LocationsView.swift
@@ -11,17 +11,10 @@ import MapKit
 struct LocationsView: View {
     
     @EnvironmentObject private var vm: LocationsViewModel
-    @State private var cameraPosition: MapCameraPosition = {
-            let region = MKCoordinateRegion(
-                center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
-                span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-            )
-            return MapCameraPosition.region(region)
-        }()
     
     var body: some View {
         ZStack {
-            Map(position: $cameraPosition)
+            Map(position: $vm.mapPosition)
                 .ignoresSafeArea()
         }
     }
